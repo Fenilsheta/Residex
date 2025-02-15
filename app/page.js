0d0ecdbcfbcd2b67d13579ae6a3dcfbce5af7a5d
Fenilsheta <139640320+Fenilsheta@users.noreply.github.com>
2025-02-15 18:56:46 +0530

    r

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import ListingMapView from "./_components/ListingMapView";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <div className="p-10">
       <ListingMapView type="Sell"/>
-      
+      <SpeedInsights/>
     </div>
   );
 }
