6710e7e00389809e70957964d3cdd10f6c86a03c
Fenilsheta <139640320+Fenilsheta@users.noreply.github.com>
2025-03-10 19:17:40 +0530

    n

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -6,13 +6,13 @@ import { LoadScript } from '@react-google-maps/api'
 
 function Provider({ children }) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <LoadScript
       googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY}
       libraries={['places']}
       >
       <Header />
-      <div className='mt-[90px]'>
+      <div className='flex-1 mt-[90px]'>
         {children}
       </div>
       <Footer/>
